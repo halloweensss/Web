5b6b32c0f2087f46c20e90763521f27b8f7062e1
Halloween <zumik12@yandex.ru>
2019-10-17 23:50:40 +0300

    +settings menu

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ $(document).ready(function () {
     //Open Search
     $("#SuggestionInput").click(function (event) {
         $(".suggestion-container-items").css('display', 'block');
-        $(".suggestion-container-items").fadeIn('slow').css('height', 'auto');
         event.stopPropagation();
     });
 
     $('body').click(function () {
-        $(".suggestion-container-items").fadeOut('500');
         $(".suggestion-container-items").css('display', 'none');
     });
 });
