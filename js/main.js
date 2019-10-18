da8698f1e1508fca0d1a2b14a1a2f7a7ea6a9727
Halloween <zumik12@yandex.ru>
2019-10-18 21:41:43 +0300

    +stylelint/eslint +fix text

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
-    //Open Search
-    $("#SuggestionInput").click(function (event) {
-        $(".suggestion-container-items").css('display', 'block');
-        event.stopPropagation();
-    });
+  // Open Search
+  $('#SuggestionInput').click(function (event) {
+    $('.suggestion-container-items').css('display', 'block')
+    event.stopPropagation()
+  })
 
-    $('body').click(function () {
-        $(".suggestion-container-items").css('display', 'none');
-    });
-});
+  $('body').click(function () {
+    $('.suggestion-container-items').css('display', 'none')
+  })
+})
+
+function grid () {
+  $('#masonry-container').masonry({
+    itemSelector: '.grid-item',
+    transitionDuration: '0.2s',
+    stagger: '0.03s'
+  })
+}
+
+$(document).imagesLoaded(function () {
+  grid()
+})
+
+$(document).ready(grid())
+$('.carousel').on('slid.bs.carousel', function () {
+  grid()
+})
